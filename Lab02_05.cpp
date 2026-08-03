e378359ee66b1829c73acd190535c13f7df47282
name <aakritibhandari62@gmail.com>
2026-08-03 14:03:31 +0545

    Return by reference in c++

---
+++ Lab02_05.cpp
@@ -0,0 +1,26 @@
+/*Return by Reference
+? Write a C++ program to return a reference to the larger of two numbers 
+entered by the user. Display the larger number in the main function.*/
+#include<iostream>
+using namespace std;
+
+int &larger_value(int &num1,int &num2)
+{
+	if(num1<num2)
+	{
+		return num2;
+	}
+	else{
+		return num1;
+	}
+}
+
+int main()
+{
+	int num1,num2;
+	cout<<"Enter the two number:\n";
+	cin>>num1>>num2;
+	int &result=larger_value(num1,num2);
+	cout<<"Larger number is:\t"<<result<<endl;
+	return 0;
+}
